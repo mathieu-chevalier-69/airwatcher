90f0ee1bb1b40cccb3241faa1898e2fdfedd71e7
raoul <raoul.el-mir@insa-lyon.fr>
2023-05-23 09:51:13 +0200

    Modification Donnees.cpp et Donnees.h

--- src/modele/Donnees.cpp
+++ src/modele/Donnees.cpp
@@ -8,38 +8,15 @@ class Donnees{
             FluxImport flux = new FluxImport();
 
             donneesCapteurs = new DonneesCapteurs();
-            donneesCapteurs.setListeCapteurs(flux.importerCapteurs());
+            donneesCapteurs.listeCapteurs = flux.importerCapteurs();
 
             donneesUtilisateurs = new DonneesUtilisateurs();
-            donneesUtilisateurs.setListeUtilisateurs(flux.importerUtilisateur());
+            donneesUtilisateurs.listeUtilisateurs = flux.importerUtilisateur();
 
             donneesPurificateurs = new DonneesPurificateurs();
-            donneesPurificateurs.setListePurificateurs(flux.importerPurificateur());
+            donneesPurificateurs.listePurificateurs = flux.importerPurificateur();
         }
-        DonneesCapteurs Donnees::getDonneesCapteurs()
-        {
-            return donneesCapteurs;
-        }
-        DonneesUtilisateurs Donnees::getDonneesUtilisateurs()
-        {
-            return donneesUtilisateurs;
-        }
-        DonneesPurificateurs Donnees::getDonneesPurificateurs()
-        {
-            return donneesPurificateurs;
-        }
-        void Donnees::setDonneesCapteurs(DonneesCapteurs dc)
-        {
-            donneesCapteurs = dc;
-        }
-        void Donnees::setDonneesUtilisateurs(DonneesUtilisateurs du)
-        {
-            donneesUtilisateurs = du;
-        }
-        void Donnees::setDonneesPurificateurs(DonneesPurificateurs dp)
-        {
-            donneesPurificateurs = dp;
-        }
+        
         Donnees::~Donnees()
         {
 
--- src/modele/Donnees.h
+++ src/modele/Donnees.h
@@ -8,14 +8,7 @@
 class Donnees{
     public:
         Donnees();
-        DonneesCapteurs getDonneesCapteurs();
-        DonneesUtilisateurs getDonneesUtilisateurs();
-        DonneesPurificateurs getDonneesPurificateurs();
-        void setDonneesCapteurs(DonneesCapteurs);
-        void setDonneesUtilisateurs(DonneesUtilisateurs);
-        void setDonneesPurificateurs(DonneesPurificateurs);
         ~Donnees();
-    private:
         DonneesCapteurs donneesCapteurs;
         DonneesUtilisateurs donneesUtilisateurs;
         DonneesPurificateurs donneesPurificateurs;
