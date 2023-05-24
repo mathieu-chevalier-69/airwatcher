9317678e650a5652974b703af5ba403e44746a3d
raoul <raoul.el-mir@insa-lyon.fr>
2023-05-24 16:24:30 +0200

    Modifcation setAttribute de Mesure

--- src/modele/Mesure.cpp
+++ src/modele/Mesure.cpp
@@ -4,22 +4,13 @@ Mesure::~Mesure(){}
 
 void Mesure::setAttribute(string id, float value)
 {
-    switch(id)
-    {
-        case "O3":
-            concentrationO3 = value;
-            break;
-        case "SO2":
-            concentrationSO2 = value;
-            break;
-        case "NO2":
-            concentrationNO2 = value;
-            break;
-        case "PM10":
-            concentrationPM10 = value;
-            break;
-        default:
-            cout << "Erreur lors de la modification de l'attribut\n";
-            break;
+    if (id == "O3"){
+        concentrationO3 = value;
+    }else if(id == "SO2"){
+        concentrationSO2 = value;
+    }else if(id == "NO2"){
+        concentrationNO2 = value;
+    }else if("PM10"){
+        concentrationPM10 = value;
     }
 }
