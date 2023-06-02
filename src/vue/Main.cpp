c8fc9a5db1c49def8361d69a2ce923fbcc81c75c
mathieu-chevalier-69 <mathieu.chevalier@insa-lyon.fr>
2023-06-02 17:51:24 +0200

    Clean main

--- src/vue/Main.cpp
+++ src/vue/Main.cpp
@@ -11,28 +11,7 @@
 
 int main()
 {
-    // Mesure m(2,1,1,1);
-    // Date d(1,2,2023);
-    // Capteur c("Hey", 1, 1, "moi");
-    // c.ajouterMesure(m , d);
-    // Date d1(1,2,2024);
-    // Mesure m1(3,1,1,1);
-    // c.ajouterMesure(m1 , d1);
-    // Date date = c.mesures.cbegin()->first;
-    // Mesure mesure = c.mesures.cbegin()->second;
-    // cout << "jour : " << date.jour << "mois : " << date.mois << "annee : " << date.annee << endl;
-    // cout << "3 :" << mesure.concentrationO3 << " 1 : " << mesure.concentrationSO2 << endl;
-
-    // Date date1 = (++c.mesures.cbegin())->first;
-    // Mesure mesure1 = (++c.mesures.cbegin())->second;
-    // cout << "jour : " << date1.jour << "mois : " << date1.mois << "annee : " << date1.annee << endl;
-    // cout << "2 :" << mesure1.concentrationO3 << " 1 : " << mesure1.concentrationSO2 << endl;
-
     Service service; 
-    // Date debut(27,12,2019);
-    // Date fin(31,12,2019);
-    // cout << "Moyenne :"<<service.obtenirStatsCapteur(service.donnees.donneesCapteurs.listeCapteurs[0], debut, fin) << endl;
-    //Test::testerCapteurDansZone();
     int choix = 0;
     do{
         cout << "0. Quitter" << endl;
