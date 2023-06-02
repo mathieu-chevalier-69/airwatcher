3d220c32edf5442efe893888385672fe27e9b602
ldelcourt <lou.delcourt@insa-lyon.fr>
2023-06-02 15:06:17 +0200

    Lourd

---
+++ src/test/Test.cpp
@@ -0,0 +1,22 @@
+#include "Test.h"
+
+bool Test::testerCapteurDansZone(){
+    string directoryPath = "./dataset/dataset-test/";
+    Donnees donnees(directoryPath);
+    Service service(donnees);
+
+    //1 degré = 119 km
+
+    Coordonnees pointCentral(0,0);
+    float rayon = 119.0;
+    Date dateDebut (1,1,2019);
+    Date dateFin (3,1,2019);
+
+    Mesure mesureTemoin(10,10,10,10);
+    Mesure mesureTest = service.voirStatsZone(pointCentral, rayon, dateDebut, dateFin);
+
+    cout << "Test : voir les statistiques sur une zone" << endl;
+    cout << "Resultats attendus : " << mesureTemoin << endl;
+    cout << "Resultats obtenus : " << mesureTest << endl;
+
+}
