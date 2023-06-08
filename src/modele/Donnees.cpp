2a34774a7d81d28f9ab39db59cadd0509f1faaf9
mathieu-chevalier-69 <mathieu.chevalier@insa-lyon.fr>
2023-06-08 15:34:22 +0200

    Modification en cas d'absence de capteur dans la rayon

--- src/modele/Donnees.cpp
+++ src/modele/Donnees.cpp
@@ -13,9 +13,9 @@ Donnees::Donnees(string cheminDossier)
     clock_t apresPurificateurs = clock();
     donneesCapteurs.listeCapteurs = FluxImport::retirerCapteursErrones(donneesCapteurs.listeCapteurs, 20.0f);
     clock_t apresCapteursErrones = clock();
-    cout << "Importation des capteurs en " << ((double) (apresCapteurs - t)) / CLOCKS_PER_SEC << "ms"<< endl;
-    cout << "Importation des purificateurs en " << ((double) (apresPurificateurs - apresCapteurs)) / CLOCKS_PER_SEC << "ms"<< endl;
-    cout << "Retrait des capteurs erronés en " << ((double) (apresCapteursErrones - apresPurificateurs)) / CLOCKS_PER_SEC << "ms"<< endl;
+    cout << "Importation des capteurs en " << ((double) (apresCapteurs - t)) / CLOCKS_PER_SEC << "s"<< endl;
+    cout << "Importation des purificateurs en " << ((double) (apresPurificateurs - apresCapteurs)) / CLOCKS_PER_SEC << "s"<< endl;
+    cout << "Retrait des capteurs erronés en " << ((double) (apresCapteursErrones - apresPurificateurs)) / CLOCKS_PER_SEC << "s"<< endl;
 
 }
 Donnees::~Donnees()
